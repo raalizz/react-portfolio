f7bef09b03b305b9b07c54cc21e99219bc2910b6
raalizz <135476716+raalizz@users.noreply.github.com>
2024-01-16 12:45:39 +0100

    I added a grid of h1 and an image and also added and styled an hr and made the site more responsive on smaller devices

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import codingPic from "./images/coding-pic.png";
 
 import "./Home.css";
 
 export default function Home() {
-  return <h1> Hello from Portfolio</h1>;
+  return (
+    <div className="Home">
+      <hr />
+      <section className="grid">
+        <div>
+          <h1>
+            {" "}
+            I'm <strong>Raghda Al-Shaikhli </strong>, a <br />
+            Front-end Developer{" "}
+          </h1>
+        </div>
+        <div>
+          <img src={codingPic} alt="coding languages" />
+        </div>
+      </section>
+    </div>
+  );
 }
