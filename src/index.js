39515357520e636e619ca87053ad7bc7913e712a
raalizz <135476716+raalizz@users.noreply.github.com>
2024-01-23 12:51:11 +0100

    I changed and adjusted the narbar.js and the index.js and added paths and links to each component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,29 @@ import ReactDOM from "react-dom/client";
 
 import Home from "./Home";
 import Navbar from "./Navbar";
+import About from "./About";
+import Projects from "./Projects";
+import Resume from "./Resume";
+import Contact from "./Contact";
 import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Navbar />
-    <Home />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </React.StrictMode>
+  </Router>
 );
 
 reportWebVitals();
