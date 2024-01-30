fe8e7398cc3eacb7f3645770317d4c79b1b4efbf
raalizz <135476716+raalizz@users.noreply.github.com>
2024-01-30 13:14:28 +0100

    I added an image and text with styling to the projects section

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,25 @@
 import React from "react";
+import NomnomProject from "./images/NomNom-Ninja-AI-project .png";
 
 import "./Projects.css";
 
 export default function Projects() {
-  return <div className="Projects">Hello from Projects page</div>;
+  return (
+    <div className="Projects">
+      <h2> Take a look at what I've been working on </h2>
+      <div className="dots"> ... </div>
+      <section className="project-1">
+        <div className="description">
+          <p> description goes here </p>
+        </div>
+        <div className="project-images">
+          <img
+            src={NomnomProject}
+            alt="AI Recipe Generator"
+            className="images"
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
