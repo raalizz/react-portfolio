d87d5d0c72889cb87619e17f179e75b29489936f
raalizz <135476716+raalizz@users.noreply.github.com>
2024-02-07 16:20:06 +0100

    I added my resume pic and added my resume on the resume section and styled it

--- src/Resume.js
+++ src/Resume.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import resumePic from "./images/resume-pic.png";
+
 import "./Resume.css";
 
 export default function Resume() {
-  return <div className="Resume">Hello from Resume page</div>;
+  return (
+    <div className="Resume">
+      <img src={resumePic} alt="my resume" className="my-resume" />
+    </div>
+  );
 }
