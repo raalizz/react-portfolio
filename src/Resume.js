dbd6c5128978af25988a86000b83e4c7705f8c8f
raalizz <135476716+raalizz@users.noreply.github.com>
2024-02-07 20:53:57 +0100

    I added a button to my resume pdf

--- src/Resume.js
+++ src/Resume.js
@@ -1,4 +1,5 @@
 import React from "react";
+import myResume from "./documents/Raghda-alizz-alshaikhli-resume.pdf";
 
 import resumePic from "./images/resume-pic.png";
 
@@ -8,6 +9,13 @@ export default function Resume() {
   return (
     <div className="Resume">
       <img src={resumePic} alt="my resume" className="my-resume" />
+      <figcaption>
+        <button>
+          <a href={myResume} rel=" noopener noreferrer" target="_blank">
+            Download Resume
+          </a>
+        </button>
+      </figcaption>
     </div>
   );
 }
