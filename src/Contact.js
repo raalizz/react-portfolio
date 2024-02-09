3f1eb35fd97f4471c59a5a759e42de028f1dbec5
raalizz <135476716+raalizz@users.noreply.github.com>
2024-02-09 12:44:04 +0100

    I added the contact form and styled it

--- src/Contact.js
+++ src/Contact.js
@@ -3,5 +3,39 @@ import React from "react";
 import "./Contact.css";
 
 export default function Contact() {
-  return <div className="Contact">Hello from Contact page</div>;
+  return (
+    <div className="Contact">
+      <div className="container">
+        <form className="contact-form">
+          <h3 className="contact-title"> GET IN TOUCH</h3>
+          <input
+            type="text"
+            className="contact-name"
+            placeholder="Name"
+            required
+          ></input>
+          <input
+            type="email"
+            className="contact-email"
+            placeholder="Email"
+            required
+          ></input>
+          <input
+            type="text"
+            className="contact-phone"
+            placeholder="Phone Number"
+            required
+          ></input>
+          <textarea
+            className="message"
+            rows="4"
+            placeholder="Hi there ..."
+          ></textarea>
+          <button type="submit" className="contact-btn">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
